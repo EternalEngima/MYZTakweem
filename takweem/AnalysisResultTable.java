c7db532e86055c59533df4129ccfdadab738b213
montazarhamoud <36597132+montazarhamoud@users.noreply.github.com>
2020-09-19 19:16:22 +0300

    RowFactory fixed

--- takweem/AnalysisResultTable.java
+++ takweem/AnalysisResultTable.java
@@ -33,42 +33,41 @@ public class AnalysisResultTable  extends myzTableView
         setPrefWidth(800);
         setPrefHeight(150);
         setColumnResizePolicy(CONSTRAINED_RESIZE_POLICY);
-        
-        setRowFactory( tmp -> 
-        {
-            TableRow<MYZOperation> row = new TableRow<>();
-            row.setOnMousePressed(event -> 
-            {
-                if (event.getClickCount() == 2 && (! row.isEmpty()) ) 
-                {
-                    MYZOperation rowData = row.getItem();
-                    myzPopup popup = new myzPopup(rowData.m_description , event.getScreenX() , event.getScreenY());
-                    if (!popup.isShowing()) 
-                        popup.show(primaryStage); 
-                }
-            });
-        return row ;
-        });
-        
-        setRowFactory(tv -> new TableRow<MYZOperation>() 
-        {
 
-
-            @Override
-            protected void updateItem(MYZOperation item , boolean empty) 
+        
+        setRowFactory((Object tv) -> 
+        {
+            return new TableRow<MYZOperation>() 
             {
-                super.updateItem(item , empty); 
-                if (item!=null && Double.valueOf(item.getM_value()) > Double.valueOf(item.getM_correctValue()) ) 
+                @Override
+                protected void updateItem(MYZOperation item , boolean empty) 
                 {
+                    super.updateItem(item , empty); 
                     
-                    setStyle("-fx-background-color:red");
-                } 
-                else if (item!=null && Double.valueOf(item.getM_value()) < Double.valueOf(item.getM_correctValue())) 
-                {
-                    setStyle("-fx-background-color:blue");
+                    //Set on mouse click row show the descreption
+                    this.setOnMousePressed(event ->
+                    {
+                        if (event.getClickCount() == 2 && (! this.isEmpty()) )
+                        {
+                            MYZOperation rowData = this.getItem();
+                            myzPopup popup = new myzPopup(rowData.m_description , event.getScreenX() , event.getScreenY()); 
+                            if (!popup.isShowing())
+                                popup.show(primaryStage);
+                        }
+                    });
+                    //Change row color depend on value
+                    if (item!=null && Double.valueOf(item.getM_value()) > Double.valueOf(item.getM_correctValue()) )
+                    {
+                        
+                        setStyle("-fx-background-color:red");
+                    }
+                    else if (item!=null && Double.valueOf(item.getM_value()) < Double.valueOf(item.getM_correctValue()))
+                    {
+                        setStyle("-fx-background-color:blue");
+                    }
+                    
                 }
-
-            }
+            };
         });
     }
 
